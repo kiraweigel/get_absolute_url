84f90948b528972d53f08e5555d6f703555cdded
Anna <kiraweigel4660@gmail.com>
2021-02-24 21:18:57 +0300

    get url

--- get_absolute_url.py
+++ get_absolute_url.py
@@ -1,6 +1,12 @@
-def get_absolute_url(domen, *args, **kwargs)
-    for i in args:
-        
+def get_absolute_url(domen, *args, **kwargs):
+    adress = domen
+    for arg in args:
+        adress += '/' + arg
+    adress += '?'
+    for k, v in kwargs.items():
+        adress += k + '=' + v + '&'
+    adress = adress[:-1]
+    return adress
 
 print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
 print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
