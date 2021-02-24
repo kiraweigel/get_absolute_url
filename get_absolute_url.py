237ea0cf8c3b4672729a0829d2278e1ceb4f27a6
Anna <kiraweigel4660@gmail.com>
2021-02-24 21:08:08 +0300

    get_url

---
+++ get_absolute_url.py
@@ -0,0 +1,6 @@
+def get_absolute_url(domen, *args, **kwargs)
+    for i in args:
+        
+
+print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
+print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='red', size='small'))
